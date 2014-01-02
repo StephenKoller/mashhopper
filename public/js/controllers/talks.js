c76cf5d9a8da1307195fc9632d0aedef0953cc10
detroitpro <eric@polerecky.com>
2014-01-02 16:55:11 -0500

    scope

--- public/js/controllers/talks.js
+++ public/js/controllers/talks.js
@@ -1,35 +1,26 @@
 'use strict';
 
 angular.module('mean.talks').controller('TalksController', ['$scope', '$routeParams', '$location', 'Global', 'Talks', 'User', function ($scope, $routeParams, $location, Global, Talks, User) {
-	
     $scope.global = Global;
-
     $scope.colors =  { '.NET' : 'dot-net', 'Cool Stuff' :'cool-stuff', 'Testing' : 'testing'};
 
-    //talk.Technology
-
-    $scope.style = function(index) {
-        var talk = $scope.talks[index];
+    $scope.style = function(talk) {
         console.log(talk);
         console.log($scope.colors[talk.Technology]);
         return $scope.colors[talk.Technology];
     };
 
-    $scope.update = function(index){
-    	var talk = $scope.talks[index];
+    $scope.update = function(talk){
     	var user = $scope.global.user;
-
     	if(_.contains(user.talks, talk.Id))
     		_.without(user.talks, talk.Id);
     	else
     		user.talks.push(talk.Id);
 
     	User.save(user);
-        //$scope.setStyle();
     };
 
-    $scope.setStyle = function(index){
-        var talk = $scope.talks[index];
+    $scope.setStyle = function(talk){
         var user = $scope.global.user;
         return _.contains(user.talks, talk.Id);
     };
