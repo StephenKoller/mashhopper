9cf4ba7e70b9658ada8428da191484479b3e170d
Dan Putman <putman.dan@gmail.com>
2014-01-02 12:23:54 -0500

    button for attending talks

--- public/js/controllers/talks.js
+++ public/js/controllers/talks.js
@@ -3,6 +3,20 @@
 angular.module('mean.talks').controller('TalksController', ['$routeParams', '$location', 'Global', 'Talks', function ($routeParams, $location, Global, Talks) {
 	var vm = this;
     vm.global = Global;
+    vm.update = function(){
+    	console.log('wired');
+    	  // var article = $scope.article;
+       //  if (!article.updated) {
+       //      article.updated = [];
+       //  }
+       //  article.updated.push(new Date().getTime());
+
+       //  article.$update(function() {
+       //      $location.path('articles/' + article._id);
+       //  });
+	
+
+    };
     Talks.query(function(data) {
     	vm.talks = data;
   	});
