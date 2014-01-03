d91d9a5efdebbff3688230fa06abc3cbc3e823d2
unknown <benjaminstrawn@gmail.com>
2014-01-03 15:17:40 -0500

    how to do mapreduce?

--- app/controllers/graphs.js
+++ app/controllers/graphs.js
@@ -20,40 +20,57 @@ exports.all = function(req, res) {
     // });
     // console.log(found);
 
-    var found = {"cols": [
-                {id: "month", label: "Month", type: "string"},
-                {id: "laptop-id", label: "Laptop", type: "number"},
-                {id: "desktop-id", label: "Desktop", type: "number"},
-                {id: "server-id", label: "Server", type: "number"},
-                {id: "cost-id", label: "Shipping", type: "number"}
-            ], "rows": [
-                {c: [
-                    {v: "January"},
-                    {v: 19, f: "42 items"},
-                    {v: 12, f: "Ony 12 items"},
-                    {v: 7, f: "7 servers"},
-                    {v: 4}
-                ]},
-                {c: [
-                    {v: "February"},
-                    {v: 13},
-                    {v: 1, f: "1 unit (Out of stock this month)"},
-                    {v: 12},
-                    {v: 2}
-                ]},
-                {c: [
-                    {v: "March"},
-                    {v: 24},
-                    {v: 5},
-                    {v: 11},
-                    {v: 6}
+    /*var found = {
+        "cols": [
+            {id: "month", label: "Month", type: "string"},
+            {id: "laptop-id", label: "Laptop", type: "number"},
+            {id: "desktop-id", label: "Desktop", type: "number"},
+            {id: "server-id", label: "Server", type: "number"},
+            {id: "cost-id", label: "Shipping", type: "number"}
+        ],
+        "rows": [
+        {c: [
+            {v: "January"},
+            {v: 19, f: "42 items"},
+            {v: 12, f: "Ony 12 items"},
+            {v: 7, f: "7 servers"},
+            {v: 4}
+        ]},
+        {c: [
+            {v: "February"},
+            {v: 13},
+            {v: 1, f: "1 unit (Out of stock this month)"},
+            {v: 12},
+            {v: 2}
+        ]},
+        {c: [
+            {v: "March"},
+            {v: 24},
+            {v: 5},
+            {v: 11},
+            {v: 6}
+        ]}
+    ]};*/
 
-                ]}
-            ]};
+    var found = {
+        "cols": [
+            {id: "month", label: "Month", type: "string"},
+            {id: "laptop-id", label: "Laptop", type: "number"}
+        ],
+        "rows": [
+        {c: [
+            {v: "January"},
+            {v: 19}
+        ]},
+        {c: [
+            {v: "February"},
+            {v: 13}
+        ]},
+        {c: [
+            {v: "March"},
+            {v: 24}
+        ]}
+    ]};
 
-
-
-     res.jsonp(found);
-             
-   
+    res.jsonp(found);
 };
