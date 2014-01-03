5f7d2d902f6a3fedd0978fb3feb0aa6685a318c6
detroitpro <eric@polerecky.com>
2014-01-03 13:48:44 -0500

    get graph

--- app/controllers/graphs.js
+++ app/controllers/graphs.js
@@ -14,42 +14,46 @@ var mongoose = require('mongoose'),
  * Graph Data.
  */
 exports.all = function(req, res) {
-    var found = [];
-    Talks.find().exec(function(err, users){
-        found = users;
-    });
-    console.log(found);
+    // var found = [];
+    // Talks.find().exec(function(err, users){
+    //     found = users;
+    // });
+    // console.log(found);
+
+    var found = {"cols": [
+                {id: "month", label: "Month", type: "string"},
+                {id: "laptop-id", label: "Laptop", type: "number"},
+                {id: "desktop-id", label: "Desktop", type: "number"},
+                {id: "server-id", label: "Server", type: "number"},
+                {id: "cost-id", label: "Shipping", type: "number"}
+            ], "rows": [
+                {c: [
+                    {v: "January"},
+                    {v: 19, f: "42 items"},
+                    {v: 12, f: "Ony 12 items"},
+                    {v: 7, f: "7 servers"},
+                    {v: 4}
+                ]},
+                {c: [
+                    {v: "February"},
+                    {v: 13},
+                    {v: 1, f: "1 unit (Out of stock this month)"},
+                    {v: 12},
+                    {v: 2}
+                ]},
+                {c: [
+                    {v: "March"},
+                    {v: 24},
+                    {v: 5},
+                    {v: 11},
+                    {v: 6}
+
+                ]}
+            ]};
+
+
+
      res.jsonp(found);
-             // chart1.data = {"cols": [
-            //     {id: "month", label: "Month", type: "string"},
-            //     {id: "laptop-id", label: "Laptop", type: "number"},
-            //     {id: "desktop-id", label: "Desktop", type: "number"},
-            //     {id: "server-id", label: "Server", type: "number"},
-            //     {id: "cost-id", label: "Shipping", type: "number"}
-            // ], "rows": [
-            //     {c: [
-            //         {v: "January"},
-            //         {v: 19, f: "42 items"},
-            //         {v: 12, f: "Ony 12 items"},
-            //         {v: 7, f: "7 servers"},
-            //         {v: 4}
-            //     ]},
-            //     {c: [
-            //         {v: "February"},
-            //         {v: 13},
-            //         {v: 1, f: "1 unit (Out of stock this month)"},
-            //         {v: 12},
-            //         {v: 2}
-            //     ]},
-            //     {c: [
-            //         {v: "March"},
-            //         {v: 24},
-            //         {v: 5},
-            //         {v: 11},
-            //         {v: 6}
-
-            //     ]}
-            // ]};
-
+             
    
 };
