209e7bee70f061bcd53488eee8fafcd0382fbd1b
Dave Mueller <dmueller@pillartechnology.com>
2014-01-30 13:26:41 -0500

    first rough pass at 'speakers' page

--- public/js/controllers/speakers.js
+++ public/js/controllers/speakers.js
@@ -5,25 +5,20 @@ angular.module('mean.speakers').controller('SpeakersController', ['$scope', '$ro
         $scope.global = Global;
 
         $scope.find = function() {
-            /*Articles.query(function(articles) {
-            $scope.articles = articles;
-        });*/
-
             $http.get('http://rest.codemash.org/api/speakers.json').success(function(speakers) {
+                $scope.speakers = speakers;
+            });
+        };
+        $scope.findAll = function(){
+            $http.get('/views/speakers/speakers.json').success(function(speakers){
                 $scope.speakers = speakers;
             });
         };
 
         $scope.findOne = function() {
-            /*Articles.get({
-            articleId: $routeParams.articleId
-        }, function(article) {
-            $scope.article = article;
-        });*/
 
             $http.get('http://rest.codemash.org/api/speakers.json/' + $routeParams.speakerName).success(function(speaker) {
                 $scope.speaker = speaker[0];
-                // console.log(speaker);
             });
         };
     }
