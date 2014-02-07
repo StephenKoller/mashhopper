7042126f12f26cfafe83dfb8d3d8b909ac6c54cb
Steve Koller <stephenkoller@gmail.com>
2014-02-07 16:10:51 -0500

    Cleaned up talks page, list view, added screenshot to landing page

--- public/js/controllers/header.js
+++ public/js/controllers/header.js
@@ -4,19 +4,24 @@ angular.module('mean.system').controller('HeaderController', ['$scope', 'Global'
 	function($scope, Global) {
 		$scope.global = Global;
 
-		$scope.menu = [{
-			'title': 'Schedule',
-			'link': 'schedule'
-		}, {
+		$scope.menu = [
+		// {
+		// 	'title': 'Schedule',
+		// 	'link': 'schedule'
+		// },
+		{
 			'title': 'Talks',
 			'link': 'talks'
-		}, {
-			'title': 'Speakers',
-			'link': 'speakers'
-		}, {
-			'title': 'Graphs',
-			'link': 'graphs'
-		}];
+		}, 
+		// {
+		// 	'title': 'Speakers',
+		// 	'link': 'speakers'
+		// }, {
+		// {
+		// 	'title': 'Graphs',
+		// 	'link': 'graphs'
+		// }
+		];
 
 		$scope.isCollapsed = false;
 	}
