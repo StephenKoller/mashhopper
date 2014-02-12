13dcccc89bccb034f55fff8ecbb59d59ecb544ed
detroitpro <eric@polerecky.com>
2014-02-11 19:23:47 -0500

    button

--- public/js/controllers/header.js
+++ public/js/controllers/header.js
@@ -3,26 +3,10 @@
 angular.module('mean.system').controller('HeaderController', ['$scope', 'Global',
 	function($scope, Global) {
 		$scope.global = Global;
-
-		$scope.menu = [
-		// {
-		// 	'title': 'Schedule',
-		// 	'link': 'schedule'
-		// },
-		{
+		$scope.menu = [{
 			'title': 'Talks',
 			'link': 'talks'
-		}, 
-		// {
-		// 	'title': 'Speakers',
-		// 	'link': 'speakers'
-		// }, {
-		// {
-		// 	'title': 'Graphs',
-		// 	'link': 'graphs'
-		// }
-		];
-
+		}];
 		$scope.isCollapsed = false;
 	}
 ]);
