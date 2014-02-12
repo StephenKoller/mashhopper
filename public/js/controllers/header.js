8376f774e8d95d807282b305c9eac83b5d7c7029
detroitpro <eric@polerecky.com>
2014-02-11 21:22:41 -0500

    schedule

--- public/js/controllers/header.js
+++ public/js/controllers/header.js
@@ -6,9 +6,6 @@ angular.module('mean.system').controller('HeaderController', ['$scope', 'Global'
 		$scope.menu = [{
 			'title': 'Talks',
 			'link': 'talks'
-		}, {
-			'title': 'Schedule',
-			'link': 'schedule'
 		}];
 		$scope.isCollapsed = true;
 	}
